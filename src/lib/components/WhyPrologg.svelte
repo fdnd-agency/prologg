8d832b32b14f4a176d8453573f71b23f4c66869d
a7lam <ahlam.adda@hva.nl>
2026-09-24 18:49:04 +0200

    #239 feat✨: html + svg's added + css center styling

---
+++ src/lib/components/WhyPrologg.svelte
@@ -0,0 +1,52 @@
+<script>
+    import lawIcon from '$lib/assets/law-icon.svg'
+    import shieldIcon from '$lib/assets/shield-icon.svg'
+</script>
+
+<section>
+    <div class="flex-icon">
+        <img src={lawIcon} alt="" width="25" height="25"/>
+        <h2>De realiteit in recht</h2>
+    </div>
+
+    <p>"Zonder verifieerbaar bewijs geen zaak. Slachtoffers verliezen hun stem wanneer foto's, locaties en werkuren niet forensisch zijn vastgelegd."</p>
+    
+    <div class="flex-icon">
+        <img src={shieldIcon} alt="" width="25" height="25"/>
+        <p>In de praktijk strandt vervolging of civiele vordering vaak op betwisting van screenshots en losse WhatsApp-berichten. Prologg borgt de bewijsketen direct vanaf het eerste moment met cryptografische zekerheid.</p>
+    </div>
+</section>
+
+<style>
+    section {
+        background-color: #FFFFFF;
+        color: #46464F;
+        padding: 30px 22px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;        
+
+        p {
+            max-width: 500px;
+            padding: 0 15px;
+        }
+    }
+
+    .flex-icon {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        gap: 10px;
+    }
+
+    @media (width >=768px) {
+        section {
+            border-radius: 12px;
+            max-width: 600px;
+            justify-self: center;
+        }
+    }
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script>
     import Button from "$lib/components/Button.svelte";
     import Hero from "$lib/components/Hero.svelte";
+    import WhyPrologg from "$lib/components/WhyPrologg.svelte";
 </script>
 
 <main>
     <Hero />
+    <WhyPrologg />
 </main>
 
 <style>
