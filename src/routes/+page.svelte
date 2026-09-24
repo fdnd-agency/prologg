35334f97f2d1c769872f2cec6dd9a787f1c157ca
MathijsN <72303334+MathijsN@users.noreply.github.com>
2026-09-24 10:08:58 +0200

    button component #242

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Button from "$lib/components/Button.svelte";
+</script>
+
+<Button
+    href="mailto:frank.kloos@fonetic.studio"
+    classes="hero-primary"
+    content="Vraag kantoordemo aan"
+/>
+<Button href="#" classes="hero-secondary" content="Bekijk de casus" />
+<Button
+    href="mailto:frank.kloos@fonetic.studio"
+    classes="nav"
+    content="Vraag kantoordemo aan"
+/>
+<Button
+    href="mailto:frank.kloos@fonetic.studio"
+    classes="footer"
+    content="frank.kloos@fonetic.studio"
+/>
